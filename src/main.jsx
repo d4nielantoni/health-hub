62e1250f773f964c5fd34e5ebc4f7c47f8e6f87f
usuario <usuario@gmail.com>
2024-06-13 09:21:47 -0300

    Adicionado o react-router-dom ao projeto e criado roteamento inicial das páginas

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Suporte from './pages/Suporte.jsx';
+import Avaliacao from './pages/Avaliacao.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children:[
+      {
+        path: "suporte",
+        element: <Suporte/>,
+      },
+      {
+        path: "avaliacao",
+        element: <Avaliacao/>
+      }
+    ]
+  },
+  
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
