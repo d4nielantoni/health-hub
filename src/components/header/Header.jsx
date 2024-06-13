498060eb13466f7a08453e5907761dd70636d37a
usuario <usuario@gmail.com>
2024-06-13 11:28:37 -0300

    Adicionado bootstrap icons, alterado header e footer

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,40 +1,40 @@
-export default function Header(){
-    return (
-    <header className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-    <a className="navbar-brand" href="#">HealthHub</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-    </header>
-    )
+import { NavLink } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+export default function Header() {
+  function getClassName({ isActive, isPending }) {
+    return isPending ? "nav-link" : (isActive ? "nav-link active" : "nav-link");
+  }
+
+  return (
+    <Navbar expand="sm" className="bg-body-tertiary">
+      <Container>
+        <NavLink to="/" className={getClassName} style={{fontSize:"1.4rem"}}>HealthHub</NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-sm-3">
+            <NavLink to="/suporte" className={getClassName}>Suporte</NavLink>
+            <NavLink to="/avaliacao" className={getClassName}>Avaliação</NavLink>
+            <NavDropdown title="Serviços" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">
+               Blog 
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.2">
+                Sessões ao vivo
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.3">
+                Recursos
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
