835dba0f5f1df1adebc24e0b330d5d47e3601460
guicruis <cruissoares@gmail.com>
2024-06-13 22:04:29 -0300

    Fiz o começo da página de login

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,17 @@
+export default function Login() {
+    return (
+    <div class="container" id="container">
+        <div class="form-container sign-up">
+            <form>
+                <h1>Criar conta</h1>
+                <div class="social-icons">
+                    <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                </div>
+            </form>
+        </div>
+    </div>
+    )
+}
