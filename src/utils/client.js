8f1ef454196676e2c3c212e1e063e90fcaa2beea
usuario <usuario@gmail.com>
2024-06-17 13:05:58 -0300

    Separado keys do codigo

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://ykmxhrevwsqeezmpwgqr.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrbXhocmV2d3NxZWV6bXB3Z3FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg2Mjc4MDgsImV4cCI6MjAzNDIwMzgwOH0.i_9tC_t7UyoxpEcHwspToJrUbrQzz50jLsvpMFG5kog";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase
