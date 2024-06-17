5c5abd32108894c268fa48ce6af959f923deef05
Daniel <danielantoni7575@gmail.com>
2024-06-17 10:16:24 -0300

    bom dia, Deus abençoe

--- src/pages/Recursos.jsx
+++ src/pages/Recursos.jsx
@@ -9,7 +9,7 @@ const Recursos = () => {
       margin: '20px'
     },
     box: {
-      flex: '1 0 45%',  // Flex-grow, flex-shrink, flex-basis
+      flex: '1 0 45%', 
       maxWidth: '45%',
       height: '350px',
       margin: '10px',
@@ -24,32 +24,47 @@ const Recursos = () => {
     title: {
       margin: 0,
       fontSize: '16px',
-      textAlign: 'center'
+      textAlign: 'left',
+      justifyContent: 'left' //titulos para esquerda
     },
     paragraph: {
       margin: '5px 0 0 0',
       fontSize: '14px',
-      textAlign: 'center'
+      textAlign: 'center', //
+      justifyContent:'center'
     }
   };
 
   return (
     <div style={styles.container}>
       <div style={styles.box}>
-        <h3 style={styles.title}>Caixa 1</h3>
-        <p style={styles.paragraph}>Informações da Caixa 1</p>
+        <h3 style={styles.title}>Grupo de Apoio</h3>
+        <p style={styles.paragraph}>Você não precisa passar por isso sozinho(a). Nossa plataforma oferece grupos de 
+suporte online moderados, proporcionando um espaço seguro e acolhedor para se 
+conectar com pessoas que estão enfrentando situações semelhantes.</p>
+        <button></button> 
       </div>
       <div style={styles.box}>
-        <h3 style={styles.title}>Caixa 2</h3>
-        <p style={styles.paragraph}>Informações da Caixa 2</p>
+        <h3 style={styles.title}>Ferramenta de Monitoramento</h3>
+        <p style={styles.paragraph}>Autoconhecimento é fundamental para cuidar da saúde mental. Pensando nisso, 
+                                    oferecemos uma variedade de ferramentas para te ajudar a monitorar seu 
+                                    bem-estar emocional e identificar padrões ao longo do tempo.</p>
+        <button></button> 
+
       </div>
       <div style={styles.box}>
-        <h3 style={styles.title}>Caixa 3</h3>
-        <p style={styles.paragraph}>Informações da Caixa 3</p>
+        <h3 style={styles.title}>Blog de Apoio</h3>
+        <p style={styles.paragraph}>Encontre pessoas que estão em situações similiares que você, assim como você
+                                    elas estão precisando de ajuda com algum problema ou observação e sua ajuda
+                                    pode ser de extrema importância.</p>
       </div>
       <div style={styles.box}>
-        <h3 style={styles.title}>Caixa 4</h3>
-        <p style={styles.paragraph}>Informações da Caixa 4</p>
+        <h3 style={styles.title}>Sessão ao vivo</h3>
+        <p style={styles.paragraph}>Encontre pessoas que estão em situações similiares que você, assim como você
+                                    elas estão precisando de ajuda com algum problema ou observação e sua ajuda
+                                    pode ser de extrema importância.</p>
+        <button></button>
+    
       </div>
     </div>
   );
