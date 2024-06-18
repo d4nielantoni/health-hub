a673c284639bf88b129311a7e5cbc25ca1893787
usuario <usuario@gmail.com>
2024-06-18 08:20:20 -0300

    resolvido conflitos

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,7 @@ function App() {
   return (
     <>
       <Header/>
-<<<<<<< HEAD
         <Outlet/>
-=======
-      <Outlet/>
->>>>>>> 8f1ef454196676e2c3c212e1e063e90fcaa2beea
       <Footer/>
     </>
   )
