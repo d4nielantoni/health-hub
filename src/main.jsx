1a614f74221d70d1675552288ae4fc8099373880
guicruis <cruissoares@gmail.com>
2024-06-17 21:47:19 -0300

    Corrigi os imports

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.css';
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Suporte from './pages/Suporte.jsx';
-import Avaliacao from './pages/Avaliacao.jsx';
+import Suporte from './pages/suporte/Suporte.jsx';
+import Avaliacao from './pages/avaliacao/Avaliacao.jsx';
 import Login from './pages/Login/Login.jsx';
 import Registro from './pages/Registro/Registro.jsx';
 import Blog from './pages/blog/Blog.jsx';
@@ -31,7 +31,6 @@ const router = createBrowserRouter([
       {
         path:"registro",
         element:<Registro/>
-        element: <Avaliacao />
       },
       {
         path: "blog",
